6c19234e466160c2b1a20f73f822781279784658
IBANEZ MARTINEZ Irene <irene.ibanez@soprasteria.com>
2022-11-10 20:31:45 +0100

    Home and Not found component

--- clicker/src/app/app.module.ts
+++ clicker/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
